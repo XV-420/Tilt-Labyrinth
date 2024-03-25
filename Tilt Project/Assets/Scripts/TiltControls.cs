ed5e01598d7e6570f4ee2b47c5ca66929078c6bf
William Tallarico <wct9667@rit.edu>
2024-03-25 16:36:33 -0400

    gave it an attempt

--- Tilt Project/Assets/Scripts/TiltControls.cs
+++ Tilt Project/Assets/Scripts/TiltControls.cs
@@ -2,9 +2,13 @@ using UnityEngine;
 
 public class TiltControls : MonoBehaviour
 {
-
-    [SerializeField] private Quaternion startRotation = new Quaternion(0, 0, 1, 0);
-
+    [Header("limit Rotation")] 
+    [SerializeField] private float maxXRotation = 45.0f;
+    [SerializeField] private float minXRotation = -45.0f;
+    [SerializeField] private float maxZRotation = 45.0f;
+    [SerializeField] private float minZRotation = -45.0f;
+    private Quaternion startRotation = new Quaternion(0, 0, 1, 0);
+    
     void Start()
     {
         //checks gyro support
@@ -14,6 +18,30 @@ public class TiltControls : MonoBehaviour
     //get the x and z rotation from Input
     void Update()
     {
-        transform.localRotation = GyroManager.Instance.GetGyroRotation() * startRotation;
+        //get input
+        transform.localRotation= GyroManager.Instance.GetGyroRotation(); //* startRotation;
+        
+        //transform.localRotation = new Quaternion(temp.x, 0.0f,
+         //-temp.y, temp.w);
+        
+        //LimitRot();
+
+    }
+
+    
+    //idk I tried
+    private void LimitRot()
+    {
+        Vector3 eulerAngles = transform.localRotation.eulerAngles;
+
+        eulerAngles.x = (eulerAngles.x > 180) ? eulerAngles.x - 360 : eulerAngles.y;
+        eulerAngles.x = Mathf.Clamp(eulerAngles.x, minXRotation, maxXRotation);
+
+        //eulerAngles.z = (eulerAngles.y > 180) ? eulerAngles.y - 180 : eulerAngles.y;
+        //eulerAngles.z = Mathf.Clamp(eulerAngles.y, minZRotation, maxZRotation);
+        
+        Quaternion change = Quaternion.Euler(eulerAngles);
+        
+        transform.localRotation = new Quaternion(change.x, 0.0f, -transform.localRotation.y, transform.localRotation.w);
     }
 }
