5d940a394ed318cfe9f88f4afcd396d3ef90fe22
William Tallarico <wct9667@rit.edu>
2024-03-27 14:52:21 -0400

    updated to fix

--- Tilt Project/Assets/Scripts/TiltControls.cs
+++ Tilt Project/Assets/Scripts/TiltControls.cs
@@ -6,7 +6,6 @@ public class TiltControls : MonoBehaviour
     {
         //checks gyro support
         GyroManager.Instance.EnableGyro();
-        Screen.orientation = ScreenOrientation.Portrait;
     }
     
     void Update()
@@ -17,20 +16,20 @@ public class TiltControls : MonoBehaviour
         // make the rotation relative to the local rotation rather than the world
         Quaternion localRotation = GyroManager.Instance.GetGyroReferenceRotation() * worldRotation;
         
-        //convert to euler
-        Vector3 euler = localRotation.eulerAngles; 
- 
-        //zrotation
-        Quaternion zRotation = Quaternion.AngleAxis(euler.z, Vector3.forward );
- 
+        Vector3 euler = localRotation.eulerAngles;
+
+
+        
+        Quaternion zRotation = Quaternion.AngleAxis(euler.z, Vector3.forward);
+        zRotation = new Quaternion(zRotation.x, zRotation.y, zRotation.z, 0.0f);
         // reverse the rotation about the z-axis
         localRotation = Quaternion.Inverse( zRotation ) * localRotation;
- 
-        // rotate about the x axis to make the y axis point the right direction
-        localRotation = Quaternion.AngleAxis(90, Vector3.right ) * localRotation;
 
-        localRotation = new Quaternion(-localRotation.x, 0.0f, localRotation.z, localRotation.w);
-      
+        localRotation = new Quaternion(localRotation.x, -localRotation.z, localRotation.y, localRotation.w);
+        transform.localRotation = localRotation; 
+        
+        
+        
         //debugging
         Vector3 up = localRotation * Vector3.up;
         Vector3 right = localRotation * Vector3.right;
@@ -39,7 +38,6 @@ public class TiltControls : MonoBehaviour
         Debug.DrawRay( transform.position, up * 100, Color.green );
         Debug.DrawRay( transform.position, right * 100, Color.red );
         Debug.DrawRay( transform.position, fwd * 100, Color.blue );
- 
-        transform.rotation = localRotation; 
+
     }
 }
