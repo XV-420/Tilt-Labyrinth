48b99be73aa7d1d51f9d0e296bc6b6a064577966
William Tallarico <wct9667@rit.edu>
2024-04-03 13:46:37 -0400

    removed gravity

--- Assets/Scripts/Ball.cs
+++ Assets/Scripts/Ball.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -10,10 +11,18 @@ public class Ball : MonoBehaviour
     [SerializeField] private float y;
 
     [SerializeField] private float z;
-    
+
+    private Rigidbody rigidBody;
+
+    private void Start()
+    {
+        rigidBody = GetComponent<Rigidbody>();
+    }
+
     //function to reset the ball to certain pos
     public void ResetBall()
     {
         transform.position = new Vector3(x, y, z);
+        rigidBody.velocity = Vector3.zero;
     }
 }
