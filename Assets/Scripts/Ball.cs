716b96e094f21ad24736814eb8aad2db3dba3c9c
William Tallarico <wct9667@rit.edu>
2024-04-08 12:52:30 -0400

    did some things

--- Assets/Scripts/Ball.cs
+++ Assets/Scripts/Ball.cs
@@ -1,12 +1,14 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
+using UnityEditor.Experimental.GraphView;
 using UnityEngine;
 
 public class Ball : MonoBehaviour
 {
     [Header("Position for where the ball is reset to when reset is called")]
-    [SerializeField] private GameObject spawnPos;
+    [SerializeField] private Transform spawnPos;
 
     private Rigidbody rigidBody;
 
@@ -18,7 +20,23 @@ public class Ball : MonoBehaviour
     //function to reset the ball to certain pos
     public void ResetBall()
     {
-        transform.position = spawnPos.transform.position;
+        transform.position = spawnPos.position;
         rigidBody.velocity = Vector3.zero;
+    }
+
+    private void OnCollisionExit(Collision other)
+    {
+        Debug.Log(other.gameObject.layer);
+        if (other.gameObject.layer == 3)
+        {
+            StartCoroutine(WaitBeforeRest());
+        }
+    }
+    
+    IEnumerator WaitBeforeRest()
+    {
+        // suspend execution for 1 second
+        yield return new WaitForSeconds(1);
+        ResetBall();
     }
 }
