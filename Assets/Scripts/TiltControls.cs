1add081126f0c0ef20b26040aa589c8c206f57d0
William Tallarico <wct9667@rit.edu>
2024-04-12 09:56:28 -0400

    small script refactoring

--- Assets/Scripts/TiltControls.cs
+++ Assets/Scripts/TiltControls.cs
@@ -4,25 +4,34 @@ using UnityEngine;
 public class TiltControls : MonoBehaviour
 {
 
-    private Rigidbody rigidbody;
+    private Rigidbody ballRigidBody;
+
+    [SerializeField] private float speed = 200.0f;
     
+    //gets the accelerometer data and uses the calibration data to modify it
+    //then applys that using applyforce to the rigidybody
     void Update()
     {
-        
+        //get the input
         Vector3 fixedTilt = Input.acceleration;
+        //calibrate
         fixedTilt = tiltReference * fixedTilt;
  
 
-
+        //z = y
         fixedTilt.z = fixedTilt.y;
+        //no y rotation so zero out
         fixedTilt.y = 0.0f;
 
         //rigidbody.AddForce(fixedTilt * 200.0f * Time.deltaTime);
-        rigidbody.AddForce(fixedTilt * 200.0f * Time.deltaTime);
+        ballRigidBody.AddForce(fixedTilt * speed * Time.deltaTime);
     }
     
     private Quaternion tiltReference;
     
+    /// <summary>
+    /// Calibrates the phone to its current orientation.
+    /// </summary>
     public void CalibrateAccelerometer()
     {
         Vector3 accelerationReference = Input.acceleration;
@@ -36,7 +45,7 @@ public class TiltControls : MonoBehaviour
     void Start()
     {
         CalibrateAccelerometer();
-        rigidbody = GetComponent<Rigidbody>();
+        ballRigidBody = GetComponent<Rigidbody>();
     }
 
 }
