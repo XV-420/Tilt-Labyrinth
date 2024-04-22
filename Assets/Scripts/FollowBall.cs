9decdb80927c1ef97202eda1309ca3ef79ebc2ca
William Tallarico <wct9667@rit.edu>
2024-04-22 10:20:12 -0400

    added camera bounce

--- Assets/Scripts/FollowBall.cs
+++ Assets/Scripts/FollowBall.cs
@@ -1,17 +1,26 @@
-using System;
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UIElements;
+using UnityEngine.PlayerLoop;
+
 
 public class FollowBall : MonoBehaviour
 {
     [SerializeField] private Transform ballTransform;
-    
+    [SerializeField] private float heightAboveBall;
+
+    private float startingYPos;
+
+    private void Start()
+    {
+        startingYPos = transform.position.y;
+    }
+
     // Update is called once per frame
     void Update()
     {
         Vector3 pos = ballTransform.position;
-        transform.position = new Vector3(pos.x, transform.position.y, pos.z);
+        if(pos.y > startingYPos)
+            pos.y = pos.y - startingYPos;
+        pos.y += heightAboveBall;
+        transform.position = new Vector3(pos.x, pos.y, pos.z);
     }
 }
