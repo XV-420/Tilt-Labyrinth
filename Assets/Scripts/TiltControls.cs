32262f3ab210d7757cd0064aa331db5904982292
William Tallarico <wct9667@rit.edu>
2024-04-29 10:09:43 -0400

    fix!

--- Assets/Scripts/TiltControls.cs
+++ Assets/Scripts/TiltControls.cs
@@ -9,15 +9,15 @@ public class TiltControls : MonoBehaviour
 
     private Rigidbody ballRigidBody;
 
-    [Header("Controls")]
-    [SerializeField] private float speed = 200.0f;
- 
-    [Header("Jump Controls")]
-    [SerializeField] private LayerMask m_LayerMask;
+    [Header("Controls")] [SerializeField] private float speed = 200.0f;
+
+    [Header("Jump Controls")] [SerializeField]
+    private LayerMask m_LayerMask;
+
     [SerializeField] private float jumpSpeed = 15.0f;
     [SerializeField] private bool jumpEnable = false;
 
-   
+
     //gets the accelerometer data and uses the calibration data to modify it
     //then applys that using applyforce to the rigidybody
     void Update()
@@ -26,72 +26,136 @@ public class TiltControls : MonoBehaviour
         Vector3 fixedTilt = Input.acceleration;
         //calibrate
         fixedTilt = tiltReference * fixedTilt;
-        
+
         //z = y
         fixedTilt = new Vector3(fixedTilt.x, fixedTilt.z, fixedTilt.y);
 
         if (jumpEnable)
         {
-            Collider[] hitColliders = Physics.OverlapBox(gameObject.transform.position, transform.localScale / 2, Quaternion.identity, m_LayerMask);
-
-            if (hitColliders.Length > 0)
+            if (!IsPhone90())
             {
-                //check if there is y input and double
-                if (fixedTilt.y > 0.0f)
+                Collider[] hitColliders = Physics.OverlapBox(gameObject.transform.position, transform.localScale / 2,
+                    Quaternion.identity, m_LayerMask);
+
+                if (hitColliders.Length > 0)
                 {
-                   ballRigidBody.velocity = new Vector3(ballRigidBody.velocity.x,jumpSpeed, ballRigidBody.velocity.z);
+                    //check if there is y input and double
+                    if (fixedTilt.y > 0.0f)
+                    {
+                        ballRigidBody.velocity =
+                            new Vector3(ballRigidBody.velocity.x, jumpSpeed, ballRigidBody.velocity.z);
+                    }
+
                 }
-                
+                else fixedTilt = Vector3.zero;
             }
-            else fixedTilt = Vector3.zero;
 
         }
+
         fixedTilt.y = 0.0f;
 
 
-        
+
         //debugging rays
         Debug.DrawRay(transform.position, fixedTilt * speed * Time.deltaTime, Color.green);
         Debug.DrawRay(transform.position, ballRigidBody.velocity, Color.red);
-        
+
         ballRigidBody.AddForce(fixedTilt * speed * Time.deltaTime);
     }
-    
+
     private Quaternion tiltReference;
-    
+
     /// <summary>
     /// Calibrates the phone to its current orientation.
     /// </summary>
     public void CalibrateAccelerometer()
     {
         Vector3 accelerationReference = Input.acceleration;
- 
+
         Quaternion rotateQuaternion = Quaternion.FromToRotation(
             new Vector3(0.0f, 0.0f, -1.0f), accelerationReference);
- 
+
         tiltReference = Quaternion.Inverse(rotateQuaternion);
+        
+        GyroManager.Instance.SetGyroReferenceRotation();
     }
-   
+
     void Start()
     {
         CalibrateAccelerometer();
         ballRigidBody = GetComponent<Rigidbody>();
+        GyroManager.Instance.EnableGyro();
     }
 
     public void ZeroVelocity()
     {
         ballRigidBody.velocity = Vector3.zero;
     }
-    
-    
+
+
 
     private void OnEnable()
     {
         uiEventChannel.OnCalibrate += CalibrateAccelerometer;
     }
+
     private void OnDisable()
     {
         uiEventChannel.OnCalibrate -= CalibrateAccelerometer;
+    }
+
+    
+    //returns true if the phone is beyond 90 degrees
+    private bool IsPhone90()
+    {
+        //get world input
+        Quaternion worldRotation = GyroManager.Instance.GetGyroRotation();
+
+        // make the rotation relative to the local rotation rather than the world
+        Quaternion localRotation = GyroManager.Instance.GetGyroReferenceRotation() * worldRotation;
+
+        Vector3 euler = localRotation.eulerAngles;
+        
+        Quaternion zRotation = Quaternion.AngleAxis(euler.z, Vector3.forward);
+
+        zRotation = new Quaternion(zRotation.x, zRotation.y, zRotation.z, 0.0f);
+
+        // reverse the rotation about the z-axis
+        localRotation = Quaternion.Inverse(zRotation) * localRotation;
+
+        localRotation = new Quaternion(localRotation.x, localRotation.z, localRotation.y, -localRotation.w);
+
+        Vector3 localRotationEuler = localRotation.eulerAngles;
+
+        //make y the same always
+        localRotationEuler.y = -localRotationEuler.normalized.y;
+        
+        //clamp and rotate
+        if (ClampAngle(localRotationEuler.x) || ClampAngle(localRotationEuler.z))
+            return true;
+
+        return false;
+    }
+
+
+
+    
+
+    
+    private bool ClampAngle(float angle)
+    {
+        if (angle >45 && angle < 315)
+        {       // if angle in the critic region...
+            if (angle < 315 && angle> 180)
+            {
+                return true;
+            }
+            else
+            {
+                return true;
+            }
+        }
+        return false;
     }
 
     
